5a654eb5afca266e2e8b900f10f93e285790a152
pgbarletta <pbarletta@gmail.com>
2019-10-25 17:27:50 -0300

    4UET cavity analysis

---
+++ run/4uet/cavidad/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+~/labo/ANA/build/ANA2 avg_4uet.pdb -c mod_alfa_cut.cfg -o vol_avg_4uet
+#~/labo/ANA/build/ANA2 avg_4uet.pdb -c mod_alfa_cut.cfg -d ../pdt/full_fit_4uet.nc -o vol_trj_4uet
+
+exit 0
